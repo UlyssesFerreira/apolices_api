3d8704f3dc8e7e8a966cc43d3d1375a7c7af11ff
UlyssesFerreira <ulyssesh.20@gmail.com>
2025-10-21 21:54:58 -0300

    iniciando controller e rotas de policies

---
+++ app/controllers/api/policies_controller.rb
@@ -0,0 +1,42 @@
+class Api::PoliciesController < ApplicationController
+  before_action :set_policy, only: [ :show ]
+
+  def index
+    policies = Policy.all
+    render json: policies
+  end
+
+  def create
+    policy = Policy.new(policy_params)
+    if policy.save
+      render json: policy, status: :created
+    else
+      render json: { errors: policy.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    if @policy
+      render json: @policy
+    else
+      render json: { error: "Policy not found" }, status: :not_found
+    end
+  end
+
+  private
+
+  def policy_params
+    params.require(:policy).permit(
+      :numero,
+      :data_emissao,
+      :inicio_vigencia,
+      :fim_vigencia,
+      :importancia_segurada,
+      :lmg
+    )
+  end
+
+  def set_policy
+    @policy = Policy.find_by(id: params[:id])
+  end
+end
